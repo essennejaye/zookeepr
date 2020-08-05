b4bd13200f7d15a95cae5cf7d0f91a2af2bcd016
essennejaye <sjeffers02@gmail.com>
2020-08-05 13:02:03 -0700

    web server created

--- server.js
+++ server.js
@@ -41,6 +41,10 @@ function filterByQuery(query, animalsArray) {
     // return the filtered results:
     return filteredResults;
 }
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
 
 // get method requires 2 arguments, first a string describing the route the client will fetch from
 // second a callback function that executes every time the route is accessed, res = response, req = request
@@ -50,7 +54,15 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
-})
+});
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.sendStatus(404);
+    }
+});
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
-})
+});
